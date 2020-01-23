204003f9da45fb07bb3892394918b1d45929c03b
Vlasios Dimitriadis <blasisd@certh.gr>
2020-01-23 10:15:12 +0200

    Updated drugs and conditions retrieval mechanism in scenarios. Translations fixes-additions

--- gentelella/app/apps.py
+++ gentelella/app/apps.py
@@ -2,6 +2,14 @@ from __future__ import unicode_literals
 
 from django.apps import AppConfig
 
+from app.retrieve_meddata import KnowledgeGraphWrapper
+
 
 class AppConfig(AppConfig):
     name = 'app'
+
+    def ready(self):
+        knw = KnowledgeGraphWrapper()
+        knw.cache_drugs()
+        knw.cache_conditions()
+
--- gentelella/app/retrieve_meddata.py
+++ gentelella/app/retrieve_meddata.py
@@ -1,12 +1,36 @@
+import json
+import time
+
 from collections import namedtuple
 
 from SPARQLWrapper import SPARQLWrapper2
 from SPARQLWrapper import JSON
 
 from django.conf import settings
+from django.core.cache import cache
+
+
+class NCObject(object):
+    def __init__(self, name, code):
+        self.name = name
+        self.code = code
+
+    def __eq__(self, other):
+        return "{}{}".format(self.name, self.code).__eq__(
+            "{}{}".format(other.name, other.code))
+
+    def __lt__(self, other):
+        return "{}{}".format(self.name, self.code).__lt__(
+            "{}{}".format(other.name, other.code))
+
+
+    def __gt__(self, other):
+        return "{}{}".format(self.name, self.code).__gt__(
+            "{}{}".format(other.name, other.code))
 
 
 class KnowledgeGraphWrapper:
+
     def __init__(self):
         self.sparql = SPARQLWrapper2(settings.SPARQL_ENDPOINT)
         self.sparql.setCredentials(user=settings.SPARQL_USERNAME,
@@ -15,7 +39,7 @@ class KnowledgeGraphWrapper:
         self.sparql.setHTTPAuth(settings.SPARQL_AUTH)
         self.sparql.setReturnFormat(JSON)
 
-    def get_drugs(self):
+    def cache_drugs(self):
         whole_query = """
         SELECT ?name, ?code WHERE {
         ?s <http://purl.bioontology.org/ontology/UATC/ATC_LEVEL> "5"^^<http://www.w3.org/2001/XMLSchema#string>.
@@ -33,26 +57,36 @@ class KnowledgeGraphWrapper:
         # # Turn drugs into objects with name and code
         # DrugStruct = namedtuple("DrugStruct", "name, code")
         # results = [DrugStruct(name=r[0].lower(), code=r[1]) for r in results]
-        results = self.sparql.query().bindings
 
-        # Turn drugs into objects with name and code
-        DrugStruct = namedtuple("DrugStruct", "name, code")
-        results = sorted([DrugStruct(name=r["name"].value.lower(), code=r["code"].value) for r in results])
+        drugs = self.sparql.query().bindings
 
-        return results
+        # DrugStruct = namedtuple("DrugStruct", "name, code")
+        # drugs = sorted([DrugStruct(name=d["name"].value.lower(), code=d["code"].value) for d in drugs])
+        drugs = sorted([NCObject(name=d["name"].value.lower(), code=d["code"].value
+                                  ) for d in drugs])
+
+        cache.set("drugs", drugs)
+
+    def get_drugs(self):
+        return cache.get("drugs")
+
+    def cache_conditions(self):
+        # Change with real service
+        with open("app/med_data/medDRA.json", "r") as fp:
+            conditions = sorted(json.load(fp).items())
+
+            # #
+            # conditions = sorted([("Pain", "10033371"), ("Stinging", "10033371"),
+            #                      ("Rhinitis", "10039083"), ("Allergic rhinitis", "10039085"),
+            #                      ("Partial visual loss", "10047571"), ("Dry mouth", "10013781"),
+            #                      ("Sprue-like enteropathy", "10079622"), ])
+
+            conditions = [NCObject(name=n, code=c) for n, c in conditions]
+
+            cache.set("conditions", conditions)
 
     def get_conditions(self):
-        # Change with real service
-        conditions = sorted([("Pain", "10033371"), ("Stinging", "10033371"),
-                             ("Rhinitis", "10039083"), ("Allergic rhinitis", "10039085"),
-                             ("Partial visual loss", "10047571"), ("Dry mouth", "10013781"),
-                             ("Sprue-like enteropathy", "10079622"), ])
-
-        # Turn conditions into objects with name and code
-        ConditionStruct = namedtuple("ConditionStruct", "name, code")
-        conditions = [ConditionStruct(name=c[0], code=c[1]) for c in conditions]
-
-        return conditions
+        return cache.get("conditions")
 
 # def get_drugs():
 #
