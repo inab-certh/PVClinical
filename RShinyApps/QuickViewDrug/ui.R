56be5f9e375a1c3c9ad175b8398787424a776760
vagelis <evgakis@yahoo.gr>
2020-04-13 11:44:23 +0300

    change graphs

--- RShinyApps/QuickViewDrug/ui.R
+++ RShinyApps/QuickViewDrug/ui.R
@@ -1,3 +1,4 @@
+library(plotly)
 library(rsconnect)
 library(shinyjs)
 library(shiny)
@@ -5,7 +6,9 @@ library(shinyWidgets)
 library(DT)
 library(shinycssloaders)
 library(shinyalert)
-
+library(dygraphs)
+library(xts)          # To make the convertion data-frame / xts format
+library(tidyverse)
 
 options(encoding = 'UTF-8')
 
@@ -77,8 +80,9 @@ shinyUI(fluidPage(includeCSS("../sharedscripts/custom.css"),
                             
                   fluidRow(useShinyjs(),
                            column(width=6,
-                                  fluidRow( withSpinner(plotOutput_p("seriousplot",HTML( tt('dot1') ), tt('dot2')))),
-                                  fluidRow( column(width=11,offset=1,withSpinner(plotOutput_p( 'cpmeanplot') )))
+                                  # fluidRow( withSpinner(plotOutput_p("seriousplot",HTML( tt('dot1') ), tt('dot2')))),
+                                  fluidRow( withSpinner(plotOutput("seriousplot"))),
+                                  fluidRow( column(width=12,withSpinner(dygraphOutput( 'cpmeanplot') )))
                                   
                                   
                                   
