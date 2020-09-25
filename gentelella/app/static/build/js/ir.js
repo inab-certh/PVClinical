05a27962a1dfa661c08ad8d3c9658de37f0c3cdf
Vlasios Dimitriadis <blasisd@certh.gr>
2020-09-25 17:13:01 +0300

    Fixed various things in ohdsi_wrappers, working ohdsi_workspace, half ready ir form

--- gentelella/app/static/build/js/ir.js
+++ gentelella/app/static/build/js/ir.js
@@ -1,4 +1,20 @@
 $(function() {
+    var study_chkbx = $("input[name='add_study_window']");
+    show_hide_study_fields(study_chkbx[0]);
 
+    study_chkbx.change(function () {
+        show_hide_study_fields(this);
+    });
 
+    function show_hide_study_fields(chkbx) {
+        console.log("okie");
+        console.log(chkbx.checked);
+        if(chkbx.checked === false) {
+            $("#studyStartDate").hide();
+            $("#studyEndDate").hide();
+        } else {
+            $("#studyStartDate").show();
+            $("#studyEndDate").show();
+        }
+    }
 });
