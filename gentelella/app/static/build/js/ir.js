6fe32350a71cfee8f6723f91c9ddd4b96911ca7b
Vlasios Dimitriadis <blasisd@certh.gr>
2020-10-01 18:02:17 +0300

    Fixed ir urls and logic, adding modals, fields visual improvements

--- gentelella/app/static/build/js/ir.js
+++ gentelella/app/static/build/js/ir.js
@@ -13,13 +13,20 @@ $(function() {
         show_hide_ext_age($(this));
     });
 
+    $('#confirmBtn').click(function() {
+        console.log("confirmed");
+    });
+
+
     function show_hide_study_fields(std_chkbx) {
         if(std_chkbx.checked === false) {
             $("#studyStartDate").hide();
             $("#studyEndDate").hide();
+            $(".calendar-icon-div").hide();
         } else {
             $("#studyStartDate").show();
             $("#studyEndDate").show();
+            $(".calendar-icon-div").show();
         }
     }
 
@@ -33,3 +40,42 @@ $(function() {
         }
     }
 });
+
+// function() {
+//     $('#confirmBtn').click(function() {
+//         console.log("confirmed");
+//         // $.ajax({
+//         //     url: "{{ request.path }}",
+//         //     method: 'DELETE',
+//         //     data: {"scenario_id": $(this).data("record")},
+//         //     beforeSend: function(xhr) {
+//         //         xhr.setRequestHeader("X-CSRFToken", $('input[name=csrfmiddlewaretoken]').val());
+//         //     }
+//         //
+//         // }) 	.always(function(){
+//         //         $('#confirmModal').modal('hide');
+//         //     })
+//         //     .done(function(data) {
+//         //         $('[class="modal-title"]').text("{% trans 'Επιτυχής διαγραφή!'%}");
+//         //         $('[class="modal-body"]').text(data);
+//         //
+//         //         /* Info modal is already open */
+//         //         $('#infoModal').on('hidden.bs.modal', function (e) {
+//         //             window.location.replace("{% url 'index' %}");
+//         //             //location.reload();
+//         //         });
+//         //
+//         //         $("#dismissBtn").on("click", function () {
+//         //             $('#infoModal').modal('hide');
+//         //         });
+//         //
+//         //     })
+//         //     .fail(function(resp){
+//         //         $('[class="modal-title"]').text("{% trans 'Αποτυχία διαγραφής!'%}");
+//         //         $('[class="modal-body"]').text(resp.responseText);
+//         //         $('#infoModal').on('hidden.bs.modal', function (e) {
+//         //             location.reload();
+//         //         });
+//         //     });
+//     });
+// }
