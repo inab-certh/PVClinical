a96017b24c95c08da9bbd171e492cff677547a44
Vlasios Dimitriadis <blasisd@certh.gr>
2020-11-13 18:46:34 +0200

    Added help modal for ir analysis results

--- gentelella/app/static/build/js/ir.js
+++ gentelella/app/static/build/js/ir.js
@@ -13,6 +13,55 @@ $(function() {
         show_hide_ext_age($(this));
     });
 
+    var isOverIFrame = false;
+    function processMouseOut() {
+        isOverIFrame = false;
+        top.focus();
+    }
+    function processMouseOver() {
+        isOverIFrame = true;
+    }
+    function processIFrameClick() {
+        if(isOverIFrame) {
+            // replace with your function
+            $(".view-info-btn").css("display", "block");
+        }
+    }
+
+    function attachOnloadEvent(func, obj) {
+        if(typeof window.addEventListener != 'undefined') {
+            window.addEventListener('load', func, false);
+        } else if (typeof document.addEventListener != 'undefined') {
+            document.addEventListener('load', func, false);
+        } else if (typeof window.attachEvent != 'undefined') {
+            window.attachEvent('onload', func);
+        } else {
+            if (typeof window.onload == 'function') {
+                var oldonload = onload;
+                window.onload = function() {
+                    oldonload();
+                    func();
+                };
+            } else {
+                window.onload = func;
+            }
+        }
+    }
+    function init() {
+        var element = document.getElementsByTagName("iframe");
+        for (var i=0; i<element.length; i++) {
+            element[i].onmouseover = processMouseOver;
+            element[i].onmouseout = processMouseOut;
+        }
+        if (typeof window.attachEvent != 'undefined') {
+            top.attachEvent('onblur', processIFrameClick);
+        }
+        else if (typeof window.addEventListener != 'undefined') {
+            top.addEventListener('blur', processIFrameClick, false);
+        }
+    }
+    attachOnloadEvent(init);
+
     $("#irSaveBtn").click(function(event) {
         event.preventDefault();
         $.ajax({ // create an AJAX call...
