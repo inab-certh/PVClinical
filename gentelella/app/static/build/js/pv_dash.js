f6be6eea495dc37e72e746ddd7da5c106e54d103
Vlasios Dimitriadis <blasisd@certh.gr>
2020-11-19 17:12:28 +0200

    Condition occurence and Drug exposure views completed

--- gentelella/app/static/build/js/pv_dash.js
+++ gentelella/app/static/build/js/pv_dash.js
@@ -7,18 +7,7 @@ $(function() {
             $("#topPVLogo").addClass("hidden-md hidden-lg").removeClass("ml-5");
             $(".navbar.nav_title").show();
         }
-    })
+    });
 
      $("[data-toggle='popover']").popover();
-
-    // $(".iframe-box iframe").hover(function () {
-    //     console.log("hover");
-    //
-    // });
-
-    // $(".fullscreen-btn").click(function (e) {
-    //     e.preventDefault();
-    //     console.log($(this).parent().parent().find("iframe"));
-    //     $(this).parent().parent().find("iframe").addClass("full-screen");
-    // });
 });
