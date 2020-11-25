92332c6fcbf9cc57ebffe537ddd20460dcf2ac4d
Vlasios Dimitriadis <blasisd@certh.gr>
2020-11-25 17:37:13 +0200

    Added notes to OHDSI workspace views

--- gentelella/app/static/build/js/pv_dash.js
+++ gentelella/app/static/build/js/pv_dash.js
@@ -10,4 +10,21 @@ $(function() {
     });
 
      $("[data-toggle='popover']").popover();
+
+     $(".notes-btn").click(function() {
+        var mod_url = $(this).data("url");
+        var mod_id = $(this).data("id");
+        $(".notesModal").attr("id", mod_id);
+        $(".notesModal iframe").attr("src", mod_url);
+        $("#"+mod_id).modal("show");
+    });
+
+    $(window).scroll(function() {
+        var winScrollTop = $(window).scrollTop();
+        var winHeight = $(window).height();
+        var floaterHeight = $('#floater').outerHeight(true);
+        var fromBottom = 350;
+        var top = winScrollTop + winHeight - floaterHeight - fromBottom;
+        $('#floater').css({'top': top + 'px'});
+    });
 });
