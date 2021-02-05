1ec962786c54be471d537d70d3866a764dacd537
sdimitsaki <sdimitsaki@certh.gr>
2021-02-05 11:16:24 +0200

    Rshiny refactor v1

---
+++ RShinyApps/RR_E_refactor/sourcedir.R
@@ -0,0 +1,11 @@
+SOURCEDIR <- '../sharedscripts/'
+if (!file.exists( paste0( SOURCEDIR, 'prr2.R') ) )
+{
+  SOURCEDIR <- 'sharedscripts/'
+}
+source( paste0( SOURCEDIR, 'helpfunctions.r') )
+source( paste0( SOURCEDIR, 'serverhelpers.R') )
+source( paste0( SOURCEDIR, 'prr2_refactor.R') )
+source( paste0( SOURCEDIR, 'uihelpers.R') )
+source( paste0( SOURCEDIR, 'jstats.R') )
+source( paste0( SOURCEDIR, 'getters_refactor.R') )
