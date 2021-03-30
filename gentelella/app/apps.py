e9fc65a84ff1f979fd9d728f658bfac3d61545ae
Vlasios Dimitriadis <blasisd@certh.gr>
2021-03-30 17:04:22 +0300

    OHDSI changes quickview initialized

--- gentelella/app/apps.py
+++ gentelella/app/apps.py
@@ -19,7 +19,7 @@ class AppConfig(AppConfig):
     def ready(self):
         knw = KnowledgeGraphWrapper()
         knw.cache_drugs()
-        # knw.cache_conditions()
+        #knw.cache_conditions()
 
         domains = ["drug", "condition"]
 
