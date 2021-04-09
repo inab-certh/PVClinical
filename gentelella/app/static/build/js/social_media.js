e0ba061a0227c7b38a46cbe1f564307c2cfeef7b
Vlasios Dimitriadis <blasisd@certh.gr>
2021-04-09 16:59:06 +0300

    Added hashed expression for collection name

--- gentelella/app/static/build/js/social_media.js
+++ gentelella/app/static/build/js/social_media.js
@@ -3,3 +3,19 @@ $(function(){
         $(this).style.height = $(this).contentWindow.document.body.scrollHeight + 'px';
     })
 });
+
+function start_data_extraction(sc_id, keywords) {
+    console.log(sc_id);
+    console.log(keywords);
+    $.ajax({
+        url: "http://83.212.101.89:8090/tethys/rest/processes/create",
+        data: {"keywords": keywords, "scenarioID": sc_id,},
+        dataType: "json",
+        success: function (data) {
+            console.log("created");
+          },
+          error: function (data) {
+            console.log("not created");
+        }
+    });
+}
