99d20bbaf28e9cfec0412563d88a8cd4826ba478
nandia.lelova <nandia.lelova@certh.gr>
2021-06-22 09:35:54 +0300

    Before merge

--- gentelella/app/forms.py
+++ gentelella/app/forms.py
@@ -405,7 +405,7 @@ class QuestionnaireForm(forms.ModelForm):
     class Meta:
         model = Questionnaire
 
-        fields = ('q1','q2','q3', 'q4','q5','q6','q7','q8','q9','q10')
+        fields = ('q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10')
 
 
     def save(self, commit=True):
